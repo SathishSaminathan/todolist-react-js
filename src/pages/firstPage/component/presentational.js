21aee23ef41122fa06c9347b266da53d3b0be76f
SathishSaminathan <Sathishcse1975@gmail.com>
2020-08-02 18:29:36 +0530

    commit for fixes

--- src/pages/firstPage/component/presentational.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import ButtonComponent from '@sharedComponent/ButtonComponent';
-
-const Presentational = ({ onIncrementClick, counterVal }) => {
-	return <ButtonComponent onClick={() => onIncrementClick(counterVal)} text="Add value" counterVal={counterVal} />;
-};
-
-export default Presentational;
